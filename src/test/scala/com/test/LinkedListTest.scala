7898a963e5bb9b461c39a54b06efb2f4663e808a
Jitendra <jitendra.kumar@knoldus.com>
2023-03-24 00:00:29 +0530

    file modified

--- src/test/scala/com/test/LinkedListTest.scala
+++ src/test/scala/com/test/LinkedListTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class LinkedListTest extends AnyFlatSpec {
 
+  //LinkedList of Int Type
   val linkedListOfInt = new LinkedList[Int]
 
   //Insertion of elements in the linked list
@@ -33,5 +34,31 @@ class LinkedListTest extends AnyFlatSpec {
   /*-----------------------------------*-------------------------------------------------*/
 
 
+  //LinkedList of String type
+  val linkedListOfString = new LinkedList[String]
+
+  //Insertion of elements in the linked list
+  it should "return true, names are inserted into the linkedlist" in {
+    assert(linkedListOfString.insert("Jitendra"))
+    assert(linkedListOfString.insert("Manish"))
+    assert(linkedListOfString.insert("Ayush"))
+    assert(linkedListOfString.insert("Ajit"))
+  }
+
+  //if currentNode will be null, means by traversing the linkedlist the pointer will be on last node
+  // so currentNode should ne null and return true
+  it should "print the elements(names) of the linkedlist and return true" in {
+    assert(linkedListOfString.traverse())
+  }
+
+  // searching "Jitendra" in the linkedlist (which is present in the linkedlist)
+  it should "return true, name is found in the linkedlist" in {
+    assert(linkedListOfString.search("Jitendra"))
+  }
+
+  // searching "Akash" in the linkedlist (which is not present in the linkedlist)
+  it should "return false, name is not found in the linkedlist" in {
+    assert(linkedListOfString.search("Akash"))
+  }
 
 }
